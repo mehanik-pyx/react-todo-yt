7da56e9ef7906514a33e485ae23ded2b5f78a89a
Alexandr Popov <mehanik-pyx@mail.ru>
2021-05-12 17:51:16 +0700

    modal using React class Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react';
 import Context from './context';
 import TodoList from './Todo/TodoList';
 import Loader from './Loader';
+import Modal from './Modal/Modal';
 
 const AddTodo = React.lazy(() => new Promise(resolve => {
     setTimeout(() => {
@@ -50,6 +51,7 @@ function App() {
         <Context.Provider value={{removeTodo}}>
             <div className="wrapper">
                 <h1>React tutorial</h1>
+                <Modal/>
 
                 <React.Suspense fallback={<p>Loading...</p>}>
                     <AddTodo onCreate={addTodo}/>
